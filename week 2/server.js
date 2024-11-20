dcc2d944a30a93d477f4302a28ef68085c8165a3
Nish245 <s223068381@deakin.edu.au>
2024-11-20 13:36:32 +1100

    Initial commit excluding node_modules

---
+++ week 2/server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+var port = process.env.port || 3000;
+
+const addTwoNumber = (n1, n2) => n1 + n2;
+const subtractTwoNumber = (n1, n2) => n1 - n2;
+
+app.get("/addTwoNumber", (req, res) => {
+    const n1 = parseInt(req.query.n1);
+    const n2 = parseInt(req.query.n2);
+    const result = addTwoNumber(n1, n2);
+    res.json({ statusCode: 200, data: result });
+});
+app.get("/subtractTwoNumber", (req, res) => {
+    console.log("Subtraction endpoint hit");
+    const n1 = parseInt(req.query.n1);
+    const n2 = parseInt(req.query.n2);
+    const result = subtractTwoNumber(n1, n2);
+    res.json({ statusCode: 200, data: result });
+});
+app.listen(port, () => {
+    console.log("App listening to: " + port);
+});
